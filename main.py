751d9f1716f7141095dff1b877041b858bd2bcb7
Robbielol <laffanhome@gmail.com>
2021-08-25 21:52:00 -0700

    Adding several changes. Mainly fixing little bugs.

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from database import db
 
 app = Flask(__name__)
+app.secret_key = "secret-key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/Camera_Tracking'
+
+db.init_app(app)
+
+loggedIn = False
 
 
-@app.route("/", methods=['post'])
+@app.route("/")
 def login():
     return render_template("Login.html")
 
 
-@app.route("/MainCamera.html", methods=['post', 'get'])
+@app.route("/Proxy.html", methods=['POST', 'GET'])
+def proxy():
+    username: str = request.form["username"]
+    session['username'] = username
+    login_authorization(username, "MainCamera.html", 'Main')
+    return redirect(url_for("mainCamera"))
+    #return login_authorization(username, "MainCamera.html", 'Main')
+
+
+@app.route("/MainCamera.html", methods=['POST', 'GET'])
 def mainCamera():
-    if request.method == 'post':
-        username = request.form["username"]
-        if username != "Rob":
-            return render_template("Login.html")
-        else:
-            return render_template("MainCamera.html", title='Main')
+    username = session['username']
+    return login_authorization(username, "MainCamera.html", 'Main')
 
 
 @app.route("/Faces.html")
 def facesPage():
-    return render_template("Faces.html", title='Faces')
+    username = session['username']
+    return login_authorization(username, "Faces.html", 'Faces ')
 
 
 @app.route("/Recordings.html")
 def RecordingsPage():
-    return render_template("Recordings.html", title='Recordings')
+    username = session['username']
+    return login_authorization(username, "Recordings.html", 'Recordings')
+
+
+def login_authorization(username, htmlPage, pageTitle):
+    result = db.engine.execute(
+        "SELECT username FROM users WHERE username='" + username + "'")# returns cursorResult object
+    if result.rowcount > 0:
+        result_username = result.first()[0]  # result rows are returned as tuples
+        if username != result_username:
+            return render_template("Login.html")
+        else:
+            return render_template(htmlPage, title=pageTitle)
+    else:
+        return render_template("Login.html")
 
 
 if __name__ == '__main__':
     app.debug = True
+    print("hi")
     app.run()
